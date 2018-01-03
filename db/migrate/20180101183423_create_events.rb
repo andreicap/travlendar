e7b861657a94d7b86887e48a4c1653fb7b81f53a
andreicap <andrei.capastru@gmail.com>
2018-01-03 12:36:02 +0100

    partially wire events pulling from google api thingie

---
+++ db/migrate/20180101183423_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :creator
+      t.datetime :start
+      t.string :status
+      t.string :link
+      t.string :calendar
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
