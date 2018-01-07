3f87cfdb83967fd19a13984ace2b6ce1ca2c47c4
andreicap <andrei.capastru@gmail.com>
2018-01-07 20:12:04 +0100

    Add model event tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-   
+
   context "valid Factory" do
     it "has a valid factory" do
       expect(FactoryBot.create(:event)).to be_valid
@@ -20,25 +20,27 @@ RSpec.describe Event, type: :model do
       expect(Event.new(creator: nil)).to_not be_valid
     end
 
-     it 'event calendar nil' do
+    it 'event calendar nil' do
       expect(Event.new(creator: nil)).to_not be_valid
     end
 
-     it 'event location nil' do
+    it 'event location nil' do
       expect(Event.new(creator: nil)).to_not be_valid
     end
 
-     it 'event start nil' do
+    it 'event start nil' do
       expect(Event.new(creator: nil)).to_not be_valid
     end
 
-     it 'event user_id nil' do
+    it 'event user_id nil' do
       expect(Event.new(creator: nil)).to_not be_valid
     end
 
-     it 'event creator user' do
+    it 'event creator user' do
       expect(Event.new(user: nil)).to_not be_valid
     end
+
+    it { should validate_uniqueness_of(:gid) }
 
   end
 
